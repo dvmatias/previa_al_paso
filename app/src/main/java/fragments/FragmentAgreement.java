90615830400021265550537a3cca440fdadb9769
cesar.delvecchio <Cesar.DelVecchioDominguez@sbdinc.com>
2018-03-26 15:21:57 -0300

    DEV-SPLASH:

--- app/src/main/java/fragments/FragmentAgreement.java
+++ app/src/main/java/fragments/FragmentAgreement.java
@@ -7,13 +7,16 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.previaalpaso.matiasdelvecchio.previaalpaso.R;
 
 import adapters.AdapterSplashPager;
 
 /**
- * Created by cesar.delvecchio on 21/03/2018.
+ * Created by cesar.delvecchio on 21/03/2018
  */
 
 public class FragmentAgreement extends Fragment {
@@ -21,6 +24,16 @@ public class FragmentAgreement extends Fragment {
      * TAG.
      */
     private static final String TAG = FragmentAgreement.class.getSimpleName();
+
+    /**
+     * Check box.
+     */
+    private CheckBox checkBoxAgreement;
+
+    /**
+     * Button.
+     */
+    private Button buttonAccept;
 
     /**
      * New instance for {@link FragmentAgreement}
@@ -47,8 +60,40 @@ public class FragmentAgreement extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_agreement, container, false);
 
-        // TODO declare and set views inside fragment.
+        checkBoxAgreement = rootView.findViewById(R.id.checkbox_agreement);
+        buttonAccept = rootView.findViewById(R.id.button_accept);
+
+        setupCheckbox();
+        setupButton();
 
         return rootView;
+    }
+
+    /**
+     * TODO (desc)
+     */
+    private void setupCheckbox() {
+        checkBoxAgreement.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setButtonEnabled(isChecked);
+            }
+        });
+    }
+
+    /**
+     * TODO (desc)
+     */
+    private void setupButton() {
+        buttonAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void setButtonEnabled(boolean isButtonEnabled) {
+        buttonAccept.setEnabled(isButtonEnabled);
     }
 }
